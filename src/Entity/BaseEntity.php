051d0033cd117f366ab077a74781af594b45550d
michaelKaefer <michael.kaefer1@gmx.at>
2020-03-14 16:45:38 +0100

    Setup base entity and base controller

---
+++ src/Entity/BaseEntity.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+abstract class BaseEntity
+{
+    /**
+     * Adds timestampable behavior
+     * Creates and updates two properties for the entity: createdAt and updatedAt.
+     */
+    use TimestampableEntity;
+
+    /**
+     * Adds translatable behavior
+     * Used locale to override Translation listener`s locale. This is not a mapped field of
+     * entity metadata, just a simple property.
+     *
+     * @Gedmo\Locale
+     */
+    private $locale;
+
+    /**
+     * Adds translatable behavior
+     *
+     * @param string $locale  A string representing a locale, for example "en", "de", "fr"
+     * @return $this
+     */
+    public function setTranslatableLocale(string $locale): self
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+}
