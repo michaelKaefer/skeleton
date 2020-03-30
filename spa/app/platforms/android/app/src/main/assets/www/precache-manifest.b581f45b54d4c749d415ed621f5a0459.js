2b252114dc224877dbd3c6896145e78b455d7f82
michaelKaefer <michael.kaefer1@gmx.at>
2020-03-30 23:05:26 +0200

    Implement app

---
+++ spa/app/platforms/android/app/src/main/assets/www/precache-manifest.b581f45b54d4c749d415ed621f5a0459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e9674386cacfae401338b5ae14edb6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "504909917b7ec0b6c152",
+    "url": "./static/css/main.a3a4fd46.chunk.css"
+  },
+  {
+    "revision": "4b9946dfaeddac6fb9db",
+    "url": "./static/js/2.17195323.chunk.js"
+  },
+  {
+    "revision": "155c253fd2465f6d4bfc92e1f6f9bb74",
+    "url": "./static/js/2.17195323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "504909917b7ec0b6c152",
+    "url": "./static/js/main.08e2257e.chunk.js"
+  },
+  {
+    "revision": "62c92428dc567c045928",
+    "url": "./static/js/runtime-main.914fe020.js"
+  }
+]);
--- spa/app/platforms/android/app/src/main/assets/www/service-worker.js
+++ spa/app/platforms/android/app/src/main/assets/www/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6d58aae83757cc5947c3482fd3abb26.js"
+  "./precache-manifest.b581f45b54d4c749d415ed621f5a0459.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
