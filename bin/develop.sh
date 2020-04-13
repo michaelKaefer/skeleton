ed0933b67a566af54baa96335dc23ccb7f08e2a1
michaelKaefer <michael.kaefer1@gmx.at>
2020-04-13 07:41:50 +0200

    Implement app

--- bin/develop.sh
+++ bin/develop.sh
@@ -2,52 +2,58 @@
 
 printf "\n"
 
-printf "1/7 Updating the symfony executable...\n"
+printf "1/8 Updating the symfony executable...\n"
 if OUTPUT="$( sudo symfony self:update )"
 then
-  printf "1/7 Success\n\n"
+  printf "1/8 Success\n\n"
 fi
 
-printf "2/7 Changing working directory to app directory...\n"
+printf "2/8 Updating composer...\n"
+if OUTPUT="$( sudo composer self:update )"
+then
+  printf "2/8 Success\n\n"
+fi
+
+printf "3/8 Changing working directory to app directory...\n"
 APP_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
 if ! cd "$APP_PATH/.."
 then
-  echo "2/7 Error on changing working directory to app directory..."
+  echo "3/8 Error on changing working directory to app directory..."
   echo "$OUTPUT"
   exit
 fi
-printf "2/7 Success, changed working directory to %s\n\n" "$APP_PATH/.."
+printf "3/8 Success, changed working directory to %s\n\n" "$APP_PATH/.."
 
-printf "3/7 Starting docker services...\n"
+printf "4/8 Starting docker services...\n"
 if ! OUTPUT="$( docker-compose up -d 2>&1 )"
 then
-  echo "3/7 Error on starting docker services:"
+  echo "4/8 Error on starting docker services:"
   echo "$OUTPUT"
   exit
 fi
-printf "3/7 Success\n\n"
+printf "4/8 Success\n\n"
 
-printf "4/7 Starting built in Symfony server...\n"
+printf "5/8 Starting built in Symfony server...\n"
 if OUTPUT="$( symfony server:start --port=8000 -d 2>&1 )"
 then
-  printf "4/7 Success\n\n"
+  printf "5/8 Success\n\n"
 else
   if echo "$OUTPUT" | grep "The local web server is already running for this project at port 8000" >/dev/null; then
-    printf "4/7 Success, server is already listening on https://127.0.0.1:8000/\n\n"
+    printf "5/8 Success, server is already listening on https://127.0.0.1:8000/\n\n"
   else
-    echo "4/7 Error on starting Symfony server:"
+    echo "5/8 Error on starting Symfony server:"
     echo "$OUTPUT"
     exit
   fi
 fi
 
-printf "5/7 Starting browser-sync for hot reloading...\n"
+printf "6/8 Starting browser-sync for hot reloading...\n"
 if [ "$(uname)" == "Darwin" ]; then
     printf "Darwin not supported"
     exit
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   if BROWSER_SYNC_PROCESSES=$(ps -eo "[PID] %p  [COMMAND] %a" --sort=start_time | grep "[b]rowser-sync start"); then
-      printf "5/7 Error on starting browser-sync: browser-sync is already running, please kill all existing browser-sync processes:\n\n"
+      printf "6/8 Error on starting browser-sync: browser-sync is already running, please kill all existing browser-sync processes:\n\n"
       printf "%s\n\n" "$BROWSER_SYNC_PROCESSES"
       printf "Example: kill -9 999999 123123 1231231\n"
       exit
@@ -57,7 +63,7 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     exit
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
   if BROWSER_SYNC_PROCESSES=$(wmic process where "name like '%node%'" get processid,commandline | grep browser); then
-      printf "5/7 Error on starting browser-sync: browser-sync is already running, please kill all existing browser-sync processes:\n\n"
+      printf "6/8 Error on starting browser-sync: browser-sync is already running, please kill all existing browser-sync processes:\n\n"
       printf "%s\n\n" "$BROWSER_SYNC_PROCESSES"
       printf "Example: taskkill -f -pid 999999 -pid 123123 -pid 1231231\n"
       exit
@@ -74,18 +80,18 @@ if ! OUTPUT="$( \
         & \
 )"
 then
-  printf "5/7 Error on starting browser-sync: \n\n%s" "$OUTPUT"
+  printf "6/8 Error on starting browser-sync: \n\n%s" "$OUTPUT"
   exit
 fi
-printf "5/7 Success, started brwoser-sync on https://127.0.0.1:3000/, for details use \"cat ./var/.browser-sync\"\n\n"
+printf "6/8 Success, started brwoser-sync on https://127.0.0.1:3000/, for details use \"cat ./var/.browser-sync\"\n\n"
 
-printf "6/7 Starting assets build watcher...\n"
+printf "7/8 Starting assets build watcher...\n"
 if [ "$(uname)" == "Darwin" ]; then
     printf "Darwin not supported"
     exit
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   if YARN_ENCORE_PROCESSES=$(ps -eo "[PID] %p  [COMMAND] %a" --sort=start_time | grep "[y]arn.js encore dev"); then
-      printf "5/7 Error on starting assets build watcher: yarn encore is already running, please kill all existing yarn encore processes:\n\n"
+      printf "7/8 Error on starting assets build watcher: yarn encore is already running, please kill all existing yarn encore processes:\n\n"
       printf "%s\n\n" "$YARN_ENCORE_PROCESSES"
       printf "Example: kill -9 999999 123123 1231231\n"
       exit
@@ -95,7 +101,7 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     exit
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
   if YARN_ENCORE_PROCESSES=$(wmic process where "name like '%node%'" get processid,commandline | grep "encore dev"); then
-      printf "5/7 Error on starting assets build watcher: yarn encore is already running, please kill all existing yarn encore processes:\n\n"
+      printf "7/8 Error on starting assets build watcher: yarn encore is already running, please kill all existing yarn encore processes:\n\n"
       printf "%s\n\n" "$YARN_ENCORE_PROCESSES"
       printf "Example: taskkill -f -pid 999999 -pid 123123 -pid 1231231\n"
       exit
@@ -103,24 +109,24 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
 fi
 if ! OUTPUT="$( yarn encore dev --watch --color &>./var/.encore-dev & )"
 then
-  echo "6/7 Error on starting assets build watcher:"
+  echo "7/8 Error on starting assets build watcher:"
   echo "$OUTPUT"
   exit
 fi
-printf "6/7 Success\n\n"
+printf "7/8 Success\n\n"
 
-printf "7/7 Starting React application in spa/...\n"
+printf "8/8 Starting React application in spa/...\n"
 if ! cd "spa/"
 then
-  echo "7/7 Error on changing working directory to spa/..."
+  echo "8/8 Error on changing working directory to spa/..."
   echo "$OUTPUT"
   exit
 fi
 if OUTPUT="$( yarn start &>../var/.react-yarn-start & )"
 then
-  printf "7/7 Success\n\n"
+  printf "8/8 Success\n\n"
 else
-  echo "7/7 Error on starting React application in spa/:"
+  echo "8/8 Error on starting React application in spa/:"
   echo "$OUTPUT"
   exit
 fi
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,12 +9,6 @@
  * file that was distributed with this source code.
  */
 
-use Symfony\Component\Dotenv\Dotenv;
+require dirname(__DIR__).'/config/bootstrap.php';
 
-require dirname(__DIR__).'/vendor/autoload.php';
-
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
+$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = 'test';
